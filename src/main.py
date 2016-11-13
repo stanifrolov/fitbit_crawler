f97aae17c4a206ec7b6af1f578ef25983cae69df
stanislavfrolov92 <mail.stanislav.frolov@gmail.com>
2016-11-13 11:22:51 +0100

    Added intraday heart rate time series
    simply changed the app from client to personal

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 import fitbit
-from settings import client_id, client_secret, access_token, refresh_token
+
+from src.settings import client_id, client_secret, access_token, refresh_token
 
 authd_client = fitbit.Fitbit(client_id,
                              client_secret,
                              access_token=access_token,
                              refresh_token=refresh_token)
 
-data = authd_client.intraday_time_series('activities/heart')
 
-print(data)
+def intraday_heart(date):
+    heart = authd_client.intraday_time_series('activities/heart', date)
+    heartrate_dataset = heart['activities-heart-intraday']['dataset']
+    heart = {}
+    for entry in heartrate_dataset:
+        heart[entry['time'].encode('ascii')] = entry['value']
+
+    return heart
+
+date = '2016-10-15'
+heartrate_data = intraday_heart(date)
+
+for d, hr in sorted(heartrate_data.items()):
+    print d, hr
+
